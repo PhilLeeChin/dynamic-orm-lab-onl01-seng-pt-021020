f32326d573ef1701cd5705e6bc4acb273e3bc8ef
Philip Lee-Chin <phil.leechin@outlook.com>
2020-04-25 15:37:05 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -23,4 +23,8 @@ class InteractiveRecord
   def initialize(choice={})
     choice.each {|property, value| self.send("#{property}=", value)}
   end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
 end
